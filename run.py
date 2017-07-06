7db03c3b5e6593cc305842a326af8f6744732d90
gdgrant <gdgrant@uchicago.edu>
2017-07-05 22:50:54 -0500

    Testing ability to run Qt application with plotting and event loop

--- run.py
+++ run.py
@@ -1,7 +1,23 @@
 from parameters import *
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
 import model
 import psutil
+import sys
 import os
+
+global data
+data = {
+    'trials'    : [],
+    'accuracy'  : []
+}
+
+def update(data_to_add):
+    data['trials'].append(data_to_add['trial'])
+    data['accuracy'].append(data_to_add['accuracy'])
+
+    curve1.setData(x=data['trials'], y=data['accuracy'])
+    app.processEvents()
 
 def switch(iteration, prev_switch_iteration, savename):
     if iteration == (prev_switch_iteration + 10):
@@ -23,10 +39,37 @@ def switch(iteration, prev_switch_iteration, savename):
     else:
         return prev_switch_iteration
 
+def stop():
+    sys.exit()
 
-def script():
+def start():
     par['df_num'] = '0004'
-    model.main(switch)
-    
+    model.main(switch, update)
 
-script()
+app = QtGui.QApplication([])
+w   = pg.GraphicsWindow()
+
+w.setWindowTitle('Realtime Network Behavior!')
+
+btn = QtGui.QPushButton('Start')
+btn2 = QtGui.QPushButton('Quit')
+txt = QtGui.QLabel('A Title, Perhaps')
+txt.setStyleSheet("QLabel { color : white; }")
+p1  = pg.PlotWidget()
+
+layout = QtGui.QGridLayout()
+w.setLayout(layout)
+
+layout.addWidget(btn, 0, 0)
+layout.addWidget(btn2, 1, 0)
+layout.addWidget(txt, 0, 1)
+layout.addWidget(p1, 1, 1)
+
+w.show()
+
+curve1 = p1.plot(x=trials, y=accuracy_array)
+
+btn.clicked.connect(start)
+btn2.clicked.connect(stop)
+
+app.exec_()
