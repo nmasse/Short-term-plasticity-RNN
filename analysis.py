7d6091684c954f24084d5bb3361d5854ce1ec494
Nicolas Masse <nicolas.masse@gmail.com>
2017-07-11 11:35:56 -0500

    Cleaned up analysis.py.

--- analysis.py
+++ analysis.py
@@ -37,89 +37,6 @@ def calculate_roc(xlist, ylist, fast_calc = False):
     return roc
 
 
-# Provide either JSON filename or category rule, target direction, and hidden_state data
-# Calculates and returns ROC curve vale
-"""
-def roc_analysis(time_stamps=[], cat_rule=[], dir_cat=[], target_dir=[], neuron=[], dendrites=[], exc=[], inh=[]):
-
-    # Pre-allocating ROC
-
-    roc_neuron, percentile_neuron, roc_dend, percentile_dend, roc_exc, percentile_exc, roc_inh, percentile_inh = [],[],[],[],[],[],[],[]
-    if par['neuron_analysis']:
-        num_hidden, num_time, batch_size = neuron.shape
-        roc_neuron = np.ones([num_hidden, len(time_stamps), batch_size, par['num_category_rule'], par['num_category_rule']]) * 0.5
-    if par['dendrites_analysis']:
-        num_hidden, num_dend, num_time, batch_size = dendrites.shape
-        roc_dend = np.ones([num_hidden, num_dend, len(time_stamps), batch_size, par['num_category_rule'], par['num_category_rule']]) * 0.5
-    if par['exc_analysis']:
-        num_hidden, num_dend, num_time, batch_size = exc.shape
-        roc_exc = np.ones([num_hidden, num_dend, len(time_stamps), batch_size, par['num_category_rule'], par['num_category_rule']]) * 0.5
-    if par['inh_analysis']:
-        num_hidden, num_dend, num_time, batch_size = inh.shape
-        roc_inh = np.ones([num_hidden, num_dend, len(time_stamps), batch_size, par['num_category_rule'], par['num_category_rule']]) * 0.5
-
-
-    # Need update based on the category rules we are using
-    # Rule 0: up == True, down == False
-    # Rule 1: left == True, right == False
-    direction_cat = np.zeros([par['num_category_rule'], batch_size])
-    for ind in range(batch_size):
-        direction_cat[0, ind] = dir_cat[ind] in [0,1,6,7]
-        direction_cat[1, ind] = dir_cat[ind] in [4,5,6,7]
-
-
-    # Get roc value
-    # roc = (num_hidden x num_dend x num_time_stamps x num_category_rule x num_category_rule)
-    t1=time.time()
-    for c1 in range(par['num_category_rule']):
-        for c2 in range(par['num_category_rule']):
-            ind0 = np.where((direction_cat[c2]==0) * (cat_rule == c1))[0]
-            if len(ind0!=0):
-                ind1 = np.where((direction_cat[c2]==1) * (cat_rule == c1))[0]
-                for n in range(num_hidden):
-                    for t in range(len(time_stamps)):
-                        if par['neuron_analysis']:
-                            roc_neuron[n, t, c1, c2]  = calculate_roc(neuron[n, time_stamps[t], ind0], neuron[n, time_stamps[t], ind1])
-                        if any([par['dendrites_analysis'], par['exc_analysis'], par['inh_analysis']]):
-                            for d in range(num_dend):
-                                if par['dendrites_analysis']:
-                                    roc_dend[n, d, t, c1, c2]  = calculate_roc(dendrites[n, d, time_stamps[t], ind0], dendrites[n, d, time_stamps[t], ind1])
-                                if par['exc_analysis']:
-                                    roc_exc[n, d, t, c1, c2]  = calculate_roc(exc[n, d, time_stamps[t], ind0], exc[n, d, time_stamps[t], ind1])
-                                if par['inh_analysis']:
-                                    roc_inh[n, d, t, c1, c2]  = calculate_roc(inh[n, d, time_stamps[t], ind0], inh[n, d, time_stamps[t], ind1])
-
-
-    # roc's = (num_hidden x num_dend x num_time_stamps x num_category_rule x num_category_rule)
-    # roc_rectified = (num_category_rule x num_category_rule x num_time_stamps x num_hidden x num_dend)
-    # percentile contains 90th percentile value of the roc_rectified
-    print('ROC DENDRITES ', time.time()-t1)
-    if par['neuron_analysis']:
-        print(roc_neuron.shape)
-        roc_neuron_rect = np.reshape(np.absolute(0.5-roc_neuron), (num_hidden*len(time_stamps)*batch_size, par['num_category_rule'], par['num_category_rule']))
-        percentile_neuron = np.zeros([par['num_category_rule'], par['num_category_rule']])
-        percentile_neuron = np.percentile(roc_neuron_rect[:,:,:], 90, axis=0)
-    if par['dendrites_analysis']:
-        roc_dend_rect = np.reshape(np.absolute(0.5-roc_dend), (num_hidden*num_dend*len(time_stamps)*batch_size,par['num_category_rule'],par['num_category_rule']))
-        percentile_dend = np.zeros([par['num_category_rule'], par['num_category_rule']])
-        percentile_dend = np.percentile(roc_dend_rect[:,:,:], 90, axis=0)
-    if par['exc_analysis']:
-        roc_dend_exc = np.reshape(np.absolute(0.5-roc_exc), (num_hidden*num_dend*len(time_stamps)*batch_size,par['num_category_rule'],par['num_category_rule']))
-        percentile_exc = np.zeros([par['num_category_rule'], par['num_category_rule']])
-        percentile_exc = np.percentile(roc_dend_exc[:,:,:], 90, axis=0)
-    if par['inh_analysis']:
-        roc_dend_inh = np.reshape(np.absolute(0.5-roc_inh), (num_hidden*num_dend*len(time_stamps)*batch_size,par['num_category_rule'],par['num_category_rule']))
-        percentile_inh = np.zeros([par['num_category_rule'], par['num_category_rule']])
-        percentile_inh = np.percentile(roc_dend_inh[:,:,:], 90, axis=0)
-
-    roc = {'neurons': (roc_neuron, percentile_neuron),
-            'dendrites': (roc_dend, percentile_dend),
-            'dendrite_exc': (roc_exc, percentile_exc),
-            'dendrite_inh': (roc_inh, percentile_inh)}
-
-    return roc
-
-"""
 def roc_analysis(trial_info, activity_hist):
 
     num_rules = trial_info['rule_index'].shape[1]
@@ -133,7 +50,6 @@ def roc_analysis(trial_info, activity_hist):
     """"
     Determine the category membership of stimuli in all receptive fields
     """
-    t1=time.time()
     for rf in range(par['num_RFs']):
         ind_rule0 = np.where(trial_info['sample_index'][:,rf]>=par['num_samples']//2)[0]
         ind_rule1 = np.where((trial_info['sample_index'][:,rf]>=par['num_samples']//4)* \
@@ -155,8 +71,8 @@ def roc_analysis(trial_info, activity_hist):
             dendrite = False
         # create variables if they're not currently in the anova dictionary
         if var not in roc.keys():
-            # TODO: Change this if using t-stat
-            roc[var] = 0.5*np.ones((dims), dtype = np.float32)
+            # TODO: Change this if using t-stat or ROC
+            roc[var] = np.zeros((dims), dtype = np.float32)
 
         for rf in range(par['num_RFs']):
             for rule in range(par['num_rules']):
@@ -185,10 +101,6 @@ def roc_analysis(trial_info, activity_hist):
 
 # Calculate and return ANOVA values based on sample inputs (directions, images, etc.)
 def anova_analysis(trial_info, activity_hist):
-
-    # REMOVE hard-coding of 1 at end
-    trial_info['sample_index'] = np.reshape(trial_info['sample_index'],(par['num_test_batches']*par['batch_train_size'], 4), order='F')
-    trial_info['rule_index'] = np.reshape(trial_info['rule_index'],(par['num_test_batches']*par['batch_train_size'], 2), order='F')
 
     num_rules = trial_info['rule_index'].shape[1]
     num_samples = trial_info['sample_index'].shape[1]
@@ -251,7 +163,7 @@ def anova_analysis(trial_info, activity_hist):
 def plot(trial_info, activity_hist):
     global iteration
     print("Plotting with iteration: ", iteration)
-    
+
     time_pts = np.array(par['time_pts'])//par['dt']
     num_trials = par['num_test_batches']*par['batch_train_size']
 
@@ -264,7 +176,7 @@ def plot(trial_info, activity_hist):
                     x = np.array([2,3,4,5,0,1,6,7])
 
                 attended_dir = trial_info['sample_index'][range(num_trials), trial_info['rule_index'][0]]
-                
+
                 mean, std, roc = [], [], []
                 for val in x:
                     data = activity_hist['state_hist'][t, i, np.where(attended_dir==val)]
@@ -297,12 +209,12 @@ def get_analysis(trial_info=[], activity_hist=[], filename=None):
     # Get analysis results
     result = {'roc': [], 'anova': []}
 
-    # if par['roc_vars'] is not None:
-        # result['roc'] = roc_analysis(trial_info, activity_hist)
-    # if par['anova_vars'] is not None:
-        # result['anova'] = anova_analysis(trial_info, activity_hist)
-    
-    plot(trial_info, activity_hist)
+    if par['roc_vars'] is not None:
+        result['roc'] = roc_analysis(trial_info, activity_hist)
+    if par['anova_vars'] is not None:
+        result['anova'] = anova_analysis(trial_info, activity_hist)
+
+    #plot(trial_info, activity_hist)
 
 
     # Save analysis result
