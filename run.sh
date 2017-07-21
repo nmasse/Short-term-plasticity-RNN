27ec9c8e5e7a92c0c2a08b2e31e5ab27d02f72cc
nmasse <nicolas.masse@gmail.com>
2017-07-21 16:31:47 -0500

    Increased requested GPUs.

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 #SBATCH --output=rnn_stp.out
 #SBATCH --error=rnn_stp.err
 #SBATCH --partition=gpu2
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --time=035:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
-#SBATCH --mem-per-cpu=2000
+#SBATCH --ntasks-per-node=4
+#SBATCH --mem-per-cpu=16000
 
 module load python/3.5.2
 python run_all_models.py
