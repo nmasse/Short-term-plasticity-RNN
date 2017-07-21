79c166883ca5baf4cf6a581cb1e42132f1625d50
nmasse <nicolas.masse@gmail.com>
2017-07-21 16:39:17 -0500

    Making GPU exclusive.

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 #SBATCH --output=rnn_stp.out
 #SBATCH --error=rnn_stp.err
 #SBATCH --partition=gpu2
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --time=035:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --nodes=4
 #SBATCH --mem-per-cpu=16000
+#SBATCH --exclusive
 
 module load python/3.5.2
 python run_all_models.py
