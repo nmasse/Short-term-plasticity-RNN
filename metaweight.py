8b5118acc2b54fea7d82443153dd257a1ad23784
viyer28 <varunviyer1@gmail.com>
2017-07-25 17:18:55 -0500

    added 'omega' functions

--- metaweight.py
+++ metaweight.py
@@ -3,3 +3,25 @@ from parameters import *
 
 def main():
     pass
+
+u = np.zeros(par['num_mw'])
+
+# Calculate new metaweights
+
+def omega(u, weight, C, g, alpha):
+
+	for i in range(len(u)):
+		if i = 0:
+			u[i] = weight
+		else:
+			u[i] = 1/C[i] * (g[i]*(u[i+1]-u[i]) - g[i-1]*(u[i]-u[i-1])) * alpha
+
+	return u
+
+# Calculate new weight, taking metaweights into consideration
+
+def omega_prime(previous_u, weight, C, g, alpha):
+
+	new_weight = g[0]/C[0]*(previous_u[1] - weight)*alpha + weight
+
+	return new_weight
