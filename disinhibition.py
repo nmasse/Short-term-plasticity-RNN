84dc4c0f9158fe7335365f00fcc91bb13ddd7405
Catherine Lee <clee04@fas.harvard.edu>
2017-08-16 15:36:26 -0500

    disinhibition circuit works at independent level, but has problems with scaling

---
+++ disinhibition.py
@@ -0,0 +1,64 @@
+import numpy as np
+import itertools
+
+def prune_connections(x, p):
+
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            if np.random.rand() > p:
+                x[i,j] = 0
+    return x
+
+def get_dend(p2, p3, p_td, p_td2):
+    n_0, n_1, n_2, n_3 = 24, 160, 160, 1
+    c_1, c_2, c_3 = 1, 1, 1
+    p_2, p_3 = p2, p3  # 1, 0.15, 1 resulted in (0, 0.0001)
+    beta = 1.0
+    scale = 5.4
+
+    num_iters = 1000
+    dend = np.zeros((num_iters, 2))
+
+    for i in range(num_iters):
+        W_vip = np.ones((n_2, n_1)) * c_2
+        W_som = np.ones((n_3, n_2)) * c_3
+        W_vip = prune_connections(W_vip, p_2)
+        W_som = prune_connections(W_som, p_3)
+
+
+        W_td = np.zeros((n_1, n_0))
+        W_td2 = np.zeros((n_2, n_0))
+        for k in range(n_0//2):
+            for m in range(n_1//2):
+                W_td[m,k] = 1
+                W_td[m+(n_1//2),k+(n_0//2)] = 1
+        for k in range(n_0//2):
+            for m in range(n_2//2):
+                W_td2[m,k] = 1
+                W_td2[m+(n_2//2),k+(n_0//2)] = 1
+        W_td = prune_connections(W_td, p_td)
+        W_td2 = prune_connections(W_td2, p_td2)
+
+
+        for j in range(2):
+            if j == 0:
+                td = np.zeros((n_0,1))
+                td[:n_0//2,0] = 2
+            else:
+                td = np.zeros((n_0,1))
+                td[n_0//2:,0] = 2
+
+            VIP = np.matmul(W_td, td)
+            vip = np.matmul(W_vip, VIP)
+            td2 = np.matmul(W_td2, td)
+            SOM = np.maximum(0,beta - vip/scale + td2)
+            dend[i,j] = np.matmul(W_som, SOM)<1
+
+    print('Dendrite analysis...')
+    print(np.mean(dend[:,0]), np.mean(dend[:,1]), np.mean(dend[:,0]*dend[:,1]), np.mean(dend[:,0])*np.mean(dend[:,1]))
+
+# p2, p3, p_td, p_td2
+# vip->som, som->dend, td->vip, td->som
+get_dend(0.275, 0.35, 0.2, 0.5)
+
+
