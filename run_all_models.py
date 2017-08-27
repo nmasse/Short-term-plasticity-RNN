eff90ba46a874d40c484804438464292512f5773
Nicolas Masse <nicolas.masse@gmail.com>
2017-08-27 10:52:03 -0500

    mistake updating params

--- run_all_models.py
+++ run_all_models.py
@@ -3,11 +3,12 @@ from parameters import *
 import model
 import sys
 
-task_list = ['ABCA']
+task_list = ['dualDMS']
 
 for task in task_list:
     j = sys.argv[1]
     print('Training network on ', task,' task, network model number ', j)
-
+    save_fn = task + '_' + str(j) + '.pkl'
+    updates = {'trial_type': task, 'save_fn': save_fn}
     update_parameters(updates)
     model.train_and_analyze()
