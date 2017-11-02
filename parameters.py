192831e4b48bd79a7d7530e63018eea841bdc659
nmasse <nicolas.masse@gmail.com>
2017-11-02 11:47:08 -0500

    Added test decoding, several analysis params

--- parameters.py
+++ parameters.py
@@ -14,43 +14,42 @@ rnd_save_suffix = np.random.randint(10000)
 
 par = {
     # Setup parameters
-    'save_dir'              : 'C:/Users/nicol/Projects/RNN STP Analysis/',
+    'save_dir'              : 'C:/Users/nicol/Projects/RNN_analysis_nov1/',
     'debug_model'           : False,
     'load_previous_model'   : False,
-    'analyze_model'         : False,
+    'analyze_model'         : True,
 
     # Network configuration
     'synapse_config'        : 'std_stf', # Full is 'std_stf'
     'exc_inh_prop'          : 0.8,       # Literature 0.8, for EI off 1
     'var_delay'             : False,
-    'catch_trials'          : False,     # Note that turning on var_delay implies catch_trials
 
     # Network shape
     'num_motion_tuned'      : 36,
     'num_fix_tuned'         : 0,
     'num_rule_tuned'        : 0,
-    'n_hidden'              : 200,
+    'n_hidden'              : 100,
     'n_output'              : 3,
 
     # Timings and rates
     'dt'                    : 10,
-    'learning_rate'         : 5e-3,
+    'learning_rate'         : 4e-2,
     'membrane_time_constant': 100,
     'connection_prob'       : 1,         # Usually 1
 
     # Variance values
-    'clip_max_grad_val'     : 0.25,
+    'clip_max_grad_val'     : 1,
     'input_mean'            : 0.0,
-    'noise_in_sd'           : 0.01,
+    'noise_in_sd'           : 0.1,
     'noise_rnn_sd'          : 0.5,
 
     # Tuning function data
     'num_motion_dirs'       : 8,
-    'tuning_height'         : 2,        # magnitutde scaling factor for von Mises
+    'tuning_height'         : 4,        # magnitutde scaling factor for von Mises
     'kappa'                 : 2,        # concentration scaling factor for von Mises
 
     # Cost parameters
-    'spike_cost'            : 0.01,
+    'spike_cost'            : 2e-2,
 
     # Synaptic plasticity specs
     'tau_fast'              : 200,
@@ -59,26 +58,26 @@ par = {
     'U_std'                 : 0.45,
 
     # Training specs
-    'batch_train_size'      : 128,
-    'num_batches'           : 8,
-    'num_iterations'        : 1000,
+    'batch_train_size'      : 1024,
+    'num_iterations'        : 4000,
     'iters_between_outputs' : 100,
 
     # Task specs
     'trial_type'            : 'DMS', # allowable types: DMS, DMRS45, DMRS90, DMRS180, DMC, DMS+DMRS, ABBA, ABCA, dualDMS
     'rotation_match'        : 0,  # angular difference between matching sample and test
-    'dead_time'             : 400,
+    'dead_time'             : 250,
     'fix_time'              : 500,
     'sample_time'           : 500,
     'delay_time'            : 1000,
     'test_time'             : 500,
-    'rule_onset_time'       : 1900,
-    'rule_offset_time'      : 2100,
+    'rule_onset_time'       : 500,
+    'rule_offset_time'      : 700,
     'variable_delay_max'    : 500,
     'mask_duration'         : 50,  # duration of traing mask after test onset
-    'catch_trial_pct'       : 0,
+    'catch_trial_pct'       : 0.0,
     'num_receptive_fields'  : 1,
     'num_rules'             : 1, # this will be two for the DMS+DMRS task
+    'decoding_test_mode'    : False,
 
     # Save paths
     'save_fn'               : 'model_results.pkl',
@@ -86,7 +85,12 @@ par = {
     'ckpt_load_fn'          : 'model' + str(rnd_save_suffix) + '.ckpt',
 
     # Analysis
-    'svm_normalize'         : True
+    'svm_normalize'         : True,
+    'decoding_reps'         : 100,
+    'simulation_reps'       : 100,
+    'decode_test'           : True,
+    'decode_rule'           : False,
+    'decode_sample_vs_test' : False,
 }
 
 """
@@ -96,26 +100,24 @@ analysis_par = {
     'analyze_model'         : True,
     'load_previous_model'   : True,
     'num_iterations'        : 1,
-    'num_batches'           : 1,
     'batch_train_size'      : 1024,
     'var_delay'             : False,
     'learning_rate'         : 0,
-    'catch_trial_pct'       : 0,
+    'catch_trial_pct'       : 0.0,
 }
 
 """
 Parameters to be used after running analysis
 """
 revert_analysis_par = {
-    'analyze_model'         : False,
+    'analyze_model'         : True,
     'load_previous_model'   : False,
-    'num_iterations'        : 1000,
-    'num_batches'           : 8,
-    'batch_train_size'      : 128,
-    'var_delay'             : True,
-    'learning_rate'         : 5e-3,
-    'catch_trial_pct'       : 0,
-    'delay_time'            : 1000
+    'num_iterations'        : 4000,
+    'batch_train_size'      : 1024,
+    'var_delay'             : False,
+    'learning_rate'         : 2e-2,
+    'catch_trial_pct'       : 0.0,
+    'decoding_test_mode'    : False
 }
 
 
@@ -128,6 +130,7 @@ def update_parameters(updates):
     Takes a list of strings and values for updating parameters in the parameter dictionary
     Example: updates = [(key, val), (key, val)]
     """
+    print('Updating parameters...')
     for key, val in updates.items():
         par[key] = val
         print(key, val)
@@ -141,11 +144,13 @@ def update_trial_params():
     Update all the trial parameters given trial_type
     """
 
-    if par['trial_type'] == 'DMS':
-        par['num_rules'] = 1
-        par['num_rule_tuned'] = 0
+    par['num_rules'] = 1
+    par['num_rule_tuned'] = 0
 
-    if par['trial_type'] == 'DMRS45':
+    if par['trial_type'] == 'DMS' or par['trial_type'] == 'DMC':
+        par['rotation_match'] = 0
+
+    elif par['trial_type'] == 'DMRS45':
         par['rotation_match'] = 45
 
     elif par['trial_type'] == 'DMRS90':
@@ -161,17 +166,21 @@ def update_trial_params():
         par['probe_trial_pct'] = 0
         par['probe_time'] = 10
         par['num_rule_tuned'] = 12
-        par['spike_cost'] = 0.005
-        #par['num_iterations'] = 1500
-
+        par['sample_time'] = 500
+        par['test_time'] = 500
+        par['delay_time'] = 1000
+        par['analyze_rule'] = True
 
     elif par['trial_type'] == 'ABBA' or par['trial_type'] == 'ABCA':
         par['catch_trial_pct'] = 0
         par['match_test_prob'] = 0.5
         par['max_num_tests'] = 3
-        par['delay_time'] = 3000
-        par['ABBA_delay'] = int(par['delay_time']/par['max_num_tests']/2)
+        par['sample_time'] = 400
+        par['delay_time'] = 2400
+        #par['spike_cost'] = 1e-2
+        par['ABBA_delay'] = par['delay_time']//par['max_num_tests']//2
         par['repeat_pct'] = 0
+        par['analyze_test'] = True
         if par['trial_type'] == 'ABBA':
             par['repeat_pct'] = 0.5
 
@@ -182,14 +191,11 @@ def update_trial_params():
         if par['trial_type'] == 'DMS+DMRS':
             par['rotation_match'] = [0, 90]
             par['rule_onset_time'] = par['dead_time']+par['fix_time']+par['sample_time'] + 500
-            par['rule_offset_time'] = par['dead_time']+par['fix_time']+par['sample_time'] + 700
+            par['rule_offset_time'] = par['dead_time']+par['fix_time']+par['sample_time'] + 750
         else:
             par['rotation_match'] = [0, 45]
             par['rule_onset_time'] = par['dead_time']
             par['rule_offset_time'] = par['dead_time']+par['fix_time']+par['sample_time']
-
-    elif par['trial_type'] == 'DMS' or par['trial_type'] == 'DMC':
-        pass
 
     else:
         print(par['trial_type'], ' not a recognized trial type')
@@ -280,7 +286,7 @@ def update_dependencies():
     # Effective synaptic weights are stronger when no short-term synaptic plasticity
     # is used, so the strength of the recurrent weights is reduced to compensate
     if par['synapse_config'] == None:
-        par['w_rnn0'] = par['w_rnn0']/(2*spectral_radius(par['w_rnn0']))
+        par['w_rnn0'] = par['w_rnn0']/(spectral_radius(par['w_rnn0']))
 
     # Initialize output weights and biases
     par['w_out0'] =initialize([par['n_output'], par['n_hidden']], par['connection_prob'])
